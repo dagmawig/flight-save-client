451d3e190a24325079d153cb3214809419e26dab
dagmawig <dgebreselasse@gmail.com>
2022-06-06 21:18:18 -0500

    added useEffect hook to import user data during login and homepage refresh

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,14 +1,56 @@
-import React from "react";
-import {  Box } from "@mui/material"
+import { Box } from "@mui/material"
 import SearchBox from "./SearchBox";
 import SearchResultBox from "./FlightResult"
-import { useAppSelector } from './hooks'
+import { useAppSelector, useAppDispatch } from './hooks'
+import { changeSavedSearch, changeLoading } from "./flightSlice";
+import { useEffect } from "react";
+import axios, { AxiosResponse } from 'axios';
 
 
 export default function Home() {
 
+
+    const resultView = useAppSelector(state => state.flight.resultView);
+
+    const dispatch = useAppDispatch();
+    const userID = localStorage.getItem("flightSave_userID");
     
-    const resultView = useAppSelector(state => state.flight.resultView)
+    // hook used to update user data by importing it from server during page loading
+    useEffect(() => {
+        const loadUserData = async (): Promise<void | AxiosResponse<any, any>> => {
+            const data = {
+                email: localStorage.getItem("flightSave_email"),
+                userID: userID,
+            }
+
+            let resp = await axios.post<any>("https://flight-save.herokuapp.com/backend/loadData/", { ...data }).catch(err => {
+                console.log("errrrr", err);
+                dispatch(changeLoading(false));
+                alert("no flight on this date!")
+            })
+            return resp;
+        }
+
+        dispatch(changeLoading(true));
+        loadUserData().then(res => {
+            if (res) {
+                if (res.data.success) {
+                    dispatch(changeSavedSearch(res.data.data.user.searchData));
+                    dispatch(changeLoading(false));
+                }
+                else {
+                    dispatch(changeLoading(false));
+                    alert(res.data.error || res.data.message);
+                }
+            }
+            else {
+                dispatch(changeLoading(false));
+                alert("Server connection error!")
+            }
+        })
+    }, [])
+
+
 
     return (
         (!resultView) ? (
@@ -16,7 +58,7 @@ export default function Home() {
                 <SearchBox />
             </Box>
         ) :
-            <Box sx={{width: "100%", my: 0, mx: "auto", padding: 0, margin: 0, height: "calc(100vh - 80px)" }}>
+            <Box sx={{ width: "100%", my: 0, mx: "auto", padding: 0, margin: 0, height: "calc(100vh - 80px)" }}>
                 <SearchResultBox />
             </Box>
     )
